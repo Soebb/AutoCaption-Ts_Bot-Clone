ee68e9330b3073ca25efb4e6a53c5e08393ac2f6
Soebb <87156166+Soebb@users.noreply.github.com>
2021-09-09 17:32:16 +0430

    Update autoedit.py

--- Plugins/autoedit.py
+++ Plugins/autoedit.py
@@ -17,42 +17,20 @@ caption_position = usercaption_position.lower()
 
 @autocaption.on_message(filters.channel & (filters.document | filters.video | filters.audio ) & ~filters.edited, group=-1)
 async def editing(bot, message):
-      caption_text = await get_caption(Config.ADMIN_ID)
+      cap = await get_caption()
       try:
-         caption_text = caption_text.caption
+         caption = cap.caption
       except:
-         caption_text = ""
+         caption = message.caption
          pass 
-      if (message.document or message.video or message.audio): 
-          if message.caption:                        
-             file_caption = f"**{message.caption}**"                
-          else:
-             file_caption = ""           
-                                                 
-      try:
-          if caption_position == "top":
-             await bot.edit_message_caption(
-                 chat_id = message.chat.id, 
-                 message_id = message.message_id,
-                 caption = caption_text + "\n" + file_caption,
-                 parse_mode = "markdown"
-             )
-          elif caption_position == "bottom":
-             await bot.edit_message_caption(
-                 chat_id = message.chat.id, 
-                 message_id = message.message_id,
-                 caption = file_caption + "\n" + caption_text,
-                 parse_mode = "markdown"
-             )
-          elif caption_position == "nil":
-             await bot.edit_message_caption(
-                 chat_id = message.chat.id,
-                 message_id = message.message_id,
-                 caption = caption_text, 
-                 parse_mode = "markdown"
-             ) 
-      except:
-          pass
-              
+      if key:
+          await 
+      await bot.edit_message_caption(
+          chat_id = message.chat.id,
+          message_id = message.message_id,
+          caption = caption, 
+          parse_mode = "markdown"
+      ) 
+      
                    
       
