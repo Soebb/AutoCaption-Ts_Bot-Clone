0c15df45d06a8901ebf51294e045c652f7207e94
Soebb <87156166+Soebb@users.noreply.github.com>
2021-09-12 02:25:04 +0430

    Update autoedit.py

--- Plugins/autoedit.py
+++ Plugins/autoedit.py
@@ -12,11 +12,8 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import FloodWait
 import re
 
-F = "a b button name | https gh"
-print(re.split(r"[\s|]+", F, 2))
-print(re.split('[× \\s]', F, maxsplit=3))
-print(re.split('[× \\s+]', F, maxsplit=3))
-print(re.split('[None x]', F, maxsplit=3))
+F = "a -10 button name | https gh"
+print(re.split(r"[\D|]+", F, 2))
 
 
 @autocaption.on_message(~filters.edited, group=-1)
