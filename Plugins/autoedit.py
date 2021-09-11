80edbacb3ec55e7022d997f682d0667965f7d1f4
Soebb <87156166+Soebb@users.noreply.github.com>
2021-09-11 21:12:12 +0430

    Update autoedit.py

--- Plugins/autoedit.py
+++ Plugins/autoedit.py
@@ -14,9 +14,9 @@ import re
 
 F = "a -b button name | https gh"
 
-print(re.split('[- \\s]', 2, F))
-print(re.split('[- \\s+]', 2, F))
-print(re.split('[- None]', 2, F))
+print(re.split('[- \\s]', 2, int(F)))
+print(re.split('[- \\s+]', 2, int(F)))
+print(re.split('[- None]', 2, int(F)))
 
 
 @autocaption.on_message(~filters.edited, group=-1)
