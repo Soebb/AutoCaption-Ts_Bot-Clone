97d0b347c91aa72bf5259de5ad50154ed3652e38
Soebb <87156166+Soebb@users.noreply.github.com>
2021-09-12 22:31:53 +0430

    Update database.py

--- database/database.py
+++ database/database.py
@@ -25,6 +25,17 @@ SESSION = start()
 
 INSERTION_LOCK = threading.RLock()
 
+class custom_button(BASE):
+    __tablename__ = "button"
+    id = Column(Integer, primary_key=True)
+    button = Column(String)
+
+    def __init__(self, id, button):
+        self.id = id
+        self.button = button
+
+custom_button.__table__.create(checkfirst=True)
+
 class custom_caption(BASE):
     __tablename__ = "caption"
     id = Column(Integer, primary_key=True)
@@ -59,5 +70,31 @@ async def get_caption(id):
     try:
         caption = SESSION.query(custom_caption).get(id)
         return caption
+    finally:
+        SESSION.close()
+
+async def update_button(id, button):
+    with INSERTION_LOCK:
+        btn = SESSION.query(custom_button).get(id)
+        if not btn:
+            btn = custom_button(id, button)
+            SESSION.add(btn)
+            SESSION.flush()
+        else:
+            SESSION.delete(btn)
+            cap = custom_button(id, button)
+            SESSION.add(btn)
+        SESSION.commit()
+
+async def del_button(id):
+    with INSERTION_LOCK:
+        msg = SESSION.query(custom_button).get(id)
+        SESSION.delete(msg)
+        SESSION.commit()
+
+async def get_button(id):
+    try:
+        caption = SESSION.query(custom_button).get(id)
+        return button
     finally:
         SESSION.close()
