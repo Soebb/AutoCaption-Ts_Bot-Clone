6ba76c49bb6f4329816b327aa6c54267120dd580
Soebb <87156166+Soebb@users.noreply.github.com>
2021-09-13 01:13:06 +0430

    Update autoedit.py

--- Plugins/autoedit.py
+++ Plugins/autoedit.py
@@ -16,7 +16,7 @@ from pyrogram.errors import FloodWait
 async def editing(bot, message):
     if (message.chat.type == "private"):
         if ("/set_cap" in message.text) and ((len(message.text.split(' ')) == 2) or (len(message.text.split(' ')) == 1)):
-            await message.reply_text("🖊️ 𝐒𝐄𝐓 𝐂𝐀𝐏𝐓𝐈𝐎𝐍 \n\nUse this command to set your own custom caption for any of your channels.\n\n👉 `/set_cap -1001448973320 My Caption`", quote = True)
+            await message.reply_text("🖊️ 𝐒𝐄𝐓 𝐂𝐀𝐏𝐓𝐈𝐎𝐍 \n\nUse this command to set custom caption for any of your channels.\n\n👉 `/set_cap -1001448973320 My Caption`", quote = True)
         elif ("/set_cap" in message.text) and (len(message.text.split(' ')) != 2) and (len(message.text.split(' ')) != 1):
             caption = message.text.split(' ', 2)[2]
             channel = message.text.split(' ', 2)[1].replace("-100", "")
@@ -27,15 +27,15 @@ async def editing(bot, message):
                 return await message.reply_text("It seems a caption already seted for this channel id, you should first use /rmv_cap command to remove the current caption and then try seting new again.")
             await message.reply_text(f"**--Your Caption--:**\n\n{caption}", quote=True)
         if ("/set_btn" in message.text) and ((len(message.text.split(' ')) == 2) or (len(message.text.split(' ')) == 1)):
-            await message.reply_text("🖊️ 𝐒𝐄𝐓 𝐂𝐀𝐏𝐓𝐈𝐎𝐍 \n\nUse this command to set your own button for any of your channels.\nSend a Button name and URL separated by ' | '\n\n👉 `/set_btn -1001448973320 Button Name | https://t.me/my_channel`", quote = True)
+            await message.reply_text("🖊️ 𝐒𝐄𝐓 BUTTON \n\nUse this command to set button for any of your channels.\nSend a Button name and URL(separated by ' | ').\n\n👉 `/set_btn -1001448973320 Channel | https://t.me/channel`", quote = True)
         elif ("/set_btn" in message.text) and (len(message.text.split(' ')) != 2) and (len(message.text.split(' ')) != 1):
             button = message.text.split(' ', 2)[2]
-            channel = message.text.split(' ', 2)[1].replace("-", "")
+            channel = message.text.split(' ', 2)[1].replace("-100", "").replace("1", "")
             try:
                 await update_button(channel, button)
             except Exception as e:
                 print(e)
-                return await message.reply_text("It seems a button already seted for this channel id, you should first use /rmv_btn command to remove the current button and then try seting new again.")
+                return await message.reply_text("⚠️\nIt seems a button already seted for this channel id.\nYou should first use /rmv_btn command to remove the current button and then try to seting new.")
             await message.reply_text(f"**--Your Button--:**\n\n{button}", quote=True)
       
         if (message.text == "/rmv_cap"):
@@ -45,7 +45,7 @@ async def editing(bot, message):
             await del_caption(channel)
             await message.reply_text("The Caption Removed Successfully.")
         if (message.text == "/rmv_btn"):
-            await message.reply_text("Use this command to remove the current button(s) of any of your channels.\n\n👉 `/rmv_btn -1001448973320`", quote = True)
+            await message.reply_text("Use this command to remove the current button of any of your channels.\n\n👉 `/rmv_btn -1001448973320`", quote = True)
         elif ("/rmv_btn" in message.text) and (len(message.text.split(' ')) != 1):
             channel = message.text.split(' ', 1)[1].replace("-", "")
             await del_button(channel)
@@ -61,7 +61,7 @@ async def editing(bot, message):
             caption = message.caption
             pass
         try:
-            channel = str(message.chat.id).replace('-', '')
+            channel = str(message.chat.id).replace('-100', '').replace('1', '')
             btn = await get_button(int(channel))
             button = btn.button
         except:
